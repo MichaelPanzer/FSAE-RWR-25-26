7a5c5abf26d9c9632bdb22a05fbb65d6749393f9
MichaelPanzer <michaelpanzer04@gmail.com>
2025-09-21 19:09:14 -1000

    added offset to pacejka lite

--- Tire_Data_Processing/magic_formula.py
+++ Tire_Data_Processing/magic_formula.py
@@ -3,18 +3,29 @@ Contains functions for various versions of the Magic Formula tire model.
 """
 import numpy as np
 
-def pacejka_lite_lateral(x, D_1, D_2, B, C, B_p):
+def mf_52():
+    return None
+
+
+
+def pacejka_lite_lateral(x, p):
     """
     5 term Pacejka-lite lateral tire model by Bill Cobb
     https://fsaettc.org/viewtopic.php?p=1777&hilit=pacejka+lite#p1777
 
-    p: arraylike of parameters [d1, d2, b, c, bp, SV]
-    x: arraylike of inputs [alpha, Fz]"""
+    x: arraylike of inputs [alpha, Fz]
+    p: arraylike of parameters [d1, d2, b, c, bp, SV]"""
     alpha, F_z = x
+
+    if len(p)==6:
+        D_1, D_2, B, C, B_p, S_v = p
+    else:
+        D_1, D_2, B, C, B_p = p
+        S_v = 0
 
     F_z = -np.abs(F_z)  # Fz must always be negative!
     d = (D_1 + D_2 / 1000 * F_z) * F_z  # peak value (normalized)
 
-    return d * np.sin((C + (B_p / 1000 * F_z)) * np.arctan(B * alpha))
+    return d * np.sin((C + (B_p / 1000 * F_z)) * np.arctan(B * alpha)) + S_v
 
 

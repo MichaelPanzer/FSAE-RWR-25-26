0b2052bfc6c9d047c0a54d48a895be466e32c373
MichaelPanzer <michaelpanzer04@gmail.com>
2025-09-22 23:45:28 -1000

    Added MF5.2 Lateral Model

--- Tire_Data_Processing/magic_formula.py
+++ Tire_Data_Processing/magic_formula.py
@@ -10,40 +10,80 @@ def mf52_Fy(x, p, scaling_factors=None):
     https://fsaettc.org/viewtopic.php?t=117
     """
     #Model Parameters
-    p_cy1, p_dy1, p_dy2, p_dy3, p_ey1, p_ey2, p_ey3, p_ey4, p_ky1, p_ky2, p_ky3, p_hy1, p_hy2, p_hy3, p_vy1, p_vy2, p_vy3, p_vy4, p_fz0 = p
+    p_cy1, p_dy1, p_dy2, p_dy3, p_ey1, p_ey2, p_ey3, p_ey4, p_ky1, p_ky2, p_ky3, p_hy1, p_hy2, p_hy3, p_vy1, p_vy2, p_vy3, p_vy4, fz0 = p
 
     #Scaling Factors
     if scaling_factors is not None:
         lam_f0, lam_cx, lam_mux, lam_ex, lam_kx, lam_hx, lam_vx, lam_cy, lam_muy, lam_ey, lam_ky, lam_hy, lam_vy, lam_gay, lam_tr, lam_res, lam_gaz, lam_xal, lam_yka, lam_vyka, lam_s, lam_sgkp, lam_sgal, lam_gyr = scaling_factors
     else:
-        lam_f0, lam_cx = lam_mux = lam_ex = lam_kx = lam_hx = lam_vx = lam_cy = lam_muy = lam_ey = lam_ky = lam_hy = lam_vy = lam_gay = lam_tr = lam_res = lam_gaz = lam_xal = lam_yka = lam_vyka = lam_s = lam_sgkp = lam_sgal = lam_gyr = 1
+        lam_f0 =lam_cx = lam_mux = lam_ex = lam_kx = lam_hx = lam_vx = lam_cy = lam_muy = lam_ey = lam_ky = lam_hy = lam_vy = lam_gay = lam_tr = lam_res = lam_gaz = lam_xal = lam_yka = lam_vyka = lam_s = lam_sgkp = lam_sgal = lam_gyr = 1
+
+
+    #Convert Model Inputs 
+    alpha, F_z, gamma = x
+    alpha = -np.radians(alpha)
+    F_z = np.abs(F_z)
+    gamma = -np.radians(gamma)
+
+
+    gamma_y = gamma * lam_gay  # 31
+    fz0pr  = fz0 * lam_f0  # 15
+    dfz    = (F_z - fz0pr) / fz0pr  # 14
+
+    #-- lateral force (pure side slip)
+    S_hy = (p_hy1+p_hy2 * dfz) * lam_hy + p_hy3 * gamma_y  # 38 lam_hy is new 
+    alpha_y = alpha + S_hy  # 30
+    C_y = p_cy1 * lam_cy  # 32
+    mu_y = (p_dy1 + p_dy2 * dfz) * (1 - p_dy3*gamma_y**2) * lam_muy  # 34
+    D_y = mu_y * F_z  # 33
+    K_y = p_ky1 * fz0 * np.sin(2 * np.arctan(F_z / (p_ky2 * fz0 * lam_f0))) * (1-p_ky3 * np.abs(gamma_y)) * lam_f0 * lam_ky  # 36
+    B_y = K_y / (C_y * D_y)  # 37
+    E_y = (p_ey1 + p_ey2 * dfz) * (1-(p_ey3 + p_ey4*gamma_y) * np.sign(alpha_y)) * lam_ey  # 35
+    S_vy = F_z * ((p_vy1 + p_vy2*dfz)*lam_vy + (p_vy3 + p_vy4 * dfz) * gamma_y) * lam_muy  # 39 this is different from pacejka
+    
+    return D_y * np.sin(C_y * np.arctan(B_y * alpha_y - E_y*(B_y * alpha_y - np.arctan(B_y * alpha_y)))) + S_vy
+
+
+"""
+def mf52_Fy(x, p, scaling_factors=None):
+    #Pure lateral MF5.2 originally developed by TNO
+    #This function has been adapted from Bill Cobb's matlab functions
+    #https://fsaettc.org/viewtopic.php?t=117
+
+    #Model Parameters
+    p_cy1, p_dy1, p_dy2, p_dy3, p_ey1, p_ey2, p_ey3, p_ey4, p_ky1, p_ky2, p_ky3, p_hy1, p_hy2, p_hy3, p_vy1, p_vy2, p_vy3, p_vy4, fz0 = p
+
+    #Scaling Factors
+    if scaling_factors is not None:
+        lam_f0, lam_cx, lam_mux, lam_ex, lam_kx, lam_hx, lam_vx, lam_cy, lam_muy, lam_ey, lam_ky, lam_hy, lam_vy, lam_gay, lam_tr, lam_res, lam_gaz, lam_xal, lam_yka, lam_vyka, lam_s, lam_sgkp, lam_sgal, lam_gyr = scaling_factors
+    else:
+        lam_f0 =lam_cx = lam_mux = lam_ex = lam_kx = lam_hx = lam_vx = lam_cy = lam_muy = lam_ey = lam_ky = lam_hy = lam_vy = lam_gay = lam_tr = lam_res = lam_gaz = lam_xal = lam_yka = lam_vyka = lam_s = lam_sgkp = lam_sgal = lam_gyr = 1
 
 
     #Model Inputs 
-    alpha, fz, gamma = x
-
+    alpha, F_z, gamma = x
     alpha = np.radians(alpha)
-    fz = np.abs(fz)
+    F_z = np.abs(F_z)
     gamma = np.radians(gamma)
 
 
     gamma_y = gamma * lam_gay  # 31
-    fz0pr  = p_fz0 * lam_f0  # 15
-    dfz    = (fz - fz0pr) / fz0pr  # 14
+    fz0pr  = fz0 * lam_f0  # 15
+    dfz    = (F_z - fz0pr) / fz0pr  # 14
 
     #-- lateral force (pure side slip)
-    shy = (p_hy1 + p_hy2 * dfz) * lam_hy + p_hy3 * gamma_y  # 38 lam_hy is new 
-    alpha_y = alpha + shy  # 30
-    cy = p_cy1 * lam_cy  # 32
-    muy = (p_dy1 + p_dy2*dfz) * (1.0 - p_dy3*gamma_y**2) * lam_muy  # 34
-    dy = muy * fz  # 33
-    ky = p_ky1 * p_fz0 * np.sin(2.0 * np.arctan(fz / (p_ky2 * p_fz0 * lam_f0))) * (1.0 - p_ky3 * np.abs(gamma_y)) * lam_f0 * lam_ky  # 36
-    by = ky / (cy * dy)  # 37
-    ey = (p_ey1 + p_ey2 * dfz) * (1.0 - (p_ey3 + p_ey4 * gamma_y) * np.sign(alpha_y)) * lam_ey  # 35
-    svy = fz * ((p_vy1 + p_vy2 * dfz) * lam_vy + (p_vy3 + p_vy4 * dfz) * gamma_y) * lam_muy  # 39
+    S_hy = (p_hy1 + p_hy2 * dfz) * lam_hy + p_hy3 * gamma_y  # 38 lam_hy is new 
+    alpha_y = alpha + S_hy  # 30
+    C_y = np.abs(p_cy1 * lam_cy)  # 32
+    mu_y = (p_dy1 + p_dy2*dfz) * (1 - p_dy3*gamma_y**2) * lam_muy  # 34
+    D_y = mu_y * F_z  # 33
+    ky = p_ky1 * fz0 * np.sin(2.0 * np.arctan(F_z / (p_ky2 * fz0 * lam_f0))) * (1.0 - p_ky3 * np.abs(gamma_y)) * lam_f0 * lam_ky  # 36
+    B_y = ky / (C_y * D_y)  # 37
+    E_y = (p_ey1 + p_ey2 * dfz) * (1.0 - (p_ey3 + p_ey4 * gamma_y) * np.sign(alpha_y)) * lam_ey  # 35
+    S_vy = F_z * ((p_vy1 + p_vy2 * dfz)*lam_vy + (p_vy3 + p_vy4 * dfz) * gamma_y) * lam_muy  # 39 this is different from pacejka
     
-    return dy * np.sin(cy * np.arctan(by * alpha_y - ey * (by * alpha_y - np.arctan(by * alpha_y)))) + svy
-
+    return D_y * np.sin(C_y * np.arctan(B_y * alpha_y - E_y*(B_y * alpha_y - np.arctan(B_y * alpha_y)))) + S_vy
+"""
 
 
 

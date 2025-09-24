8d88cedec5d37b33f767c35279011302120c44f2
MichaelPanzer <michaelpanzer04@gmail.com>
2025-09-24 12:08:38 -1000

    Fixed mf6.1 with change to S_hy

--- Tire_Data_Processing/magic_formula.py
+++ Tire_Data_Processing/magic_formula.py
@@ -18,10 +18,13 @@ def mf61_Fy(x, p, scaling_factors=None, coordinate_system = 'adapted SAE'):
         lam_fz0, lam_cx, lam_mux, lam_ex, lam_kx, lam_hx, lam_vx, lam_cy, lam_muy, lam_ey, lam_kyalpha, lam_hy, lam_vy, lam_gay, lam_tr, lam_res, lam_gaz, lam_xal, lam_yka, lam_vyka, lam_s, lam_sgkp, lam_sgal, lam_gyr, lam_kygamma = scaling_factors
     else:
         lam_fz0 =lam_cx = lam_mux = lam_ex = lam_kx = lam_hx = lam_vx = lam_cy = lam_muy = lam_ey = lam_kyalpha = lam_hy = lam_vy = lam_gay = lam_tr = lam_res = lam_gaz = lam_xal = lam_yka = lam_vyka = lam_s = lam_sgkp = lam_sgal = lam_gyr = lam_kygamma = 1
+    
+    epsilon = 0.001
 
 
     #TODO ADD coordinate system transformations
 
+        
     #Convert Model Inputs 
     alpha, F_z, gamma, p_i = x
     alpha = -np.radians(alpha)#NOTE this - sign is to convert the original 6.1 formula from adapted SAE to SAE coordinates
@@ -30,27 +33,31 @@ def mf61_Fy(x, p, scaling_factors=None, coordinate_system = 'adapted SAE'):
     gamma = np.radians(gamma)
 
     #gamma_y = gamma * lam_gay  
-    fz0pr  = f_z0 * lam_fz0  
+    fz0pr  = f_z0 * lam_fz0 # 4.E1
 
     dfz = (F_z - fz0pr) / fz0pr  # 4.E2a
-    dpi = (p_i-p_i0)/p_i0
+    dpi = (p_i-p_i0)/p_i0 # 4.E2b
+
+    gamma_star = np.sin(gamma) # E4.4
 
     C_y = np.abs(p_cy1 * lam_cy)  # 4.E21 (>0) NOTE the abs might not be necessary but I think it is
 
-    mu_y = (p_dy1 + p_dy2*dfz) * (1 + p_dy3*dpi + p_py4*dpi**2) *(1 - p_dy3*gamma**2) * lam_muy  # 4.E23
+    mu_y = (p_dy1 + p_dy2*dfz) * (1 + p_dy3*dpi + p_py4*dpi**2) * (1 - p_dy3*gamma_star**2) * lam_muy  # 4.E23
     D_y = mu_y * F_z  # 4.E22
     
-    K_yalpha = p_ky1* f_z0* (1 + p_py1*dpi)* (1-p_ky3*np.abs(gamma)) * np.sin( p_ky4* np.arctan( F_z / (f_z0*(p_ky2+p_ky5*gamma**2)*(1+p_py2*dpi)) ) )  * lam_kyalpha  # 4.E25
+    K_yalpha = p_ky1* fz0pr* (1 + p_py1*dpi)* (1-p_ky3*np.abs(gamma_star)) * np.sin( p_ky4* np.arctan(F_z / (fz0pr*(p_ky2 + p_ky5*gamma_star**2)*(1 + p_py2*dpi))) )  * lam_kyalpha  # 4.E25
     B_y = K_yalpha / (C_y * D_y)  # 4.E26 NOTE there might need to be a +epsilon_y in the denominator but I think that can be assumed to be 0
 
-    S_vygamma = F_z * (p_vy3 + p_vy4*dfz) * gamma * lam_kygamma*lam_muy
-    S_vy = F_z * (p_ky6 + p_ky7*dfz) * (1+p_py5*dpi) + lam_kygamma # 4.E29
+    S_vygamma = F_z * (p_vy3 + p_vy4*dfz) * gamma_star * lam_kygamma*lam_muy # E4.28
+    S_vy = F_z*(p_vy1 + p_vy2*dfz)*lam_vy*lam_muy  + S_vygamma# 4.E29
     K_ygamma0 = F_z*(p_ky6 + p_ky7*dfz) * (1 + p_py5*dpi) * lam_kygamma# 4.E30
 
-    S_hy = (p_hy1 + p_hy2*dfz) * lam_hy + (K_ygamma0*gamma-S_vygamma)/(K_yalpha) -1  # 4.E27
+    #S_hy = (p_hy1 + p_hy2*dfz) * lam_hy + (K_ygamma0*gamma_star-S_vygamma)/(K_yalpha) -1  # 4.E27
+    S_hy     =   (p_hy1 + p_hy2*dfz)*lam_hy +p_hy3*gamma #(4.E27) NOTE I took this bit of code from here Its https://github.com/jmcavalcante/MFPy/blob/main/mfpy/equations.py#L33, its different from the book but it correlates so idk
     alpha_y = alpha + S_hy  # 4.E20
 
-    E_y = (p_ey1 + p_ey2*dfz) * (1 + p_ey5*gamma**2 - (p_ey3 + p_ey4*gamma)*np.sign(alpha_y)) * lam_ey  # 4.E24 (<=1)
+    E_y = (p_ey1 + p_ey2*dfz) * (1 + p_ey5*gamma_star**2 - (p_ey3 + p_ey4*gamma_star)*np.sign(alpha_y)) * lam_ey  # 4.E24 (<=1)
+    #E_y = [np.max(1.0,ey) for ey in E_y] #NOTE vectorized max function ill try this if it still doesn't work
 
     
     return D_y * np.sin(C_y * np.arctan(B_y*alpha_y - E_y*(B_y*alpha_y - np.arctan(B_y*alpha_y)))) + S_vy # 4.E19

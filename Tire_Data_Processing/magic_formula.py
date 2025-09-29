5a711897c4bd80a4eb623d4928e3423f7f674f85
MichaelPanzer <michaelpanzer04@gmail.com>
2025-09-28 18:00:14 -1000

    Added acceleration simulation

--- Tire_Data_Processing/magic_formula.py
+++ Tire_Data_Processing/magic_formula.py
@@ -19,7 +19,6 @@ def mf61_Fy(x, p, scaling_factors=None, coordinate_system = 'adapted SAE'):
     else:
         lam_fz0 = lam_cy = lam_muy = lam_ey = lam_kyalpha = lam_hy = lam_vy = lam_kygamma = 1
     
-    epsilon = 0.00
 
 
     #TODO ADD coordinate system transformations
@@ -38,28 +37,54 @@ def mf61_Fy(x, p, scaling_factors=None, coordinate_system = 'adapted SAE'):
     dfz = (F_z - fz0pr) / fz0pr  # 4.E2a
     dpi = (p_i-p_i0)/p_i0 # 4.E2b
 
-    gamma_star = np.sin(gamma) # E4.4
-
     C_y = np.abs(p_cy1 * lam_cy)  # 4.E21 (>0) NOTE the abs might not be necessary but I think it is
 
-    mu_y = (p_dy1 + p_dy2*dfz) * (1 + p_dy3*dpi + p_py4*dpi**2) * (1 - p_dy3*gamma_star**2) * lam_muy  # 4.E23
+    mu_y = (p_dy1 + p_dy2*dfz) * (1 + p_dy3*dpi + p_py4*dpi**2) * (1 - p_dy3*gamma**2) * lam_muy  # 4.E23
     D_y = mu_y * F_z  # 4.E22
     
-    K_yalpha = p_ky1* fz0pr* (1 + p_py1*dpi)* (1-p_ky3*np.abs(gamma_star)) * np.sin( p_ky4* np.arctan(F_z / (fz0pr*(p_ky2 + p_ky5*gamma_star**2)*(1 + p_py2*dpi))) )  * lam_kyalpha  # 4.E25
+    K_yalpha = p_ky1* fz0pr* (1 + p_py1*dpi)* (1-p_ky3*np.abs(gamma)) * np.sin( p_ky4* np.arctan(F_z / (fz0pr*(p_ky2 + p_ky5*gamma**2)*(1 + p_py2*dpi))) )  * lam_kyalpha  # 4.E25
     B_y = K_yalpha / (C_y * D_y)  # 4.E26 NOTE there might need to be a +epsilon_y in the denominator but I think that can be assumed to be 0
 
-    S_vygamma = F_z * (p_vy3 + p_vy4*dfz) * gamma_star * lam_kygamma*lam_muy # E4.28
+    S_vygamma = F_z * (p_vy3 + p_vy4*dfz) * gamma * lam_kygamma*lam_muy # E4.28
     S_vy = F_z*(p_vy1 + p_vy2*dfz)*lam_vy*lam_muy  + S_vygamma# 4.E29
     K_ygamma0 = F_z*(p_ky6 + p_ky7*dfz) * (1 + p_py5*dpi) * lam_kygamma# 4.E30
 
-    S_hy = (p_hy1 + p_hy2*dfz) * lam_hy + (K_ygamma0*gamma_star-S_vygamma)/K_yalpha  # 4.E27
+    S_hy = (p_hy1 + p_hy2*dfz) * lam_hy + (K_ygamma0*gamma-S_vygamma)/K_yalpha  # 4.E27
     alpha_y = alpha + S_hy  # 4.E20
 
-    E_y = (p_ey1 + p_ey2*dfz) * (1 + p_ey5*gamma_star**2 - (p_ey3 + p_ey4*gamma_star)*np.sign(alpha_y)) * lam_ey  # 4.E24 (<=1)
+    E_y = (p_ey1 + p_ey2*dfz) * (1 + p_ey5*gamma**2 - (p_ey3 + p_ey4*gamma)*np.sign(alpha_y)) * lam_ey  # 4.E24 (<=1)
     #E_y = [np.max(1.0,ey) for ey in E_y] #NOTE vectorized max function ill try this if it still doesn't work
 
     
     return D_y * np.sin(C_y * np.arctan(B_y*alpha_y - E_y*(B_y*alpha_y - np.arctan(B_y*alpha_y)))) + S_vy # 4.E19
+
+def mf61_Mz(x, p, scaling_factors=None):
+    """
+    Self-Aligning torque from pure side slip MF6.1 originally developed by TNO
+    The equations below come from Tire and Vehicle Dynamics by Hans Pacejka 
+    """
+    #Model Parameters
+    q_bz1, q_bz2, q_bz3, q_bz4, q_bz5, q_cz1, q_dz1, q_dz2, q_pz1, q_hz1, q_hz2, q_hz3, q_hz4= p
+
+    #Scaling Factors
+    if scaling_factors is not None:
+        lam_fz0, lam_cy, lam_muy, lam_ey, lam_kyalpha, lam_hy, lam_vy, lam_kygamma = scaling_factors
+    else:
+        lam_fz0 = lam_cy = lam_muy = lam_ey = lam_kyalpha = lam_hy = lam_vy = lam_kygamma = 1
+
+    #TODO ADD coordinate system transformations
+    #Convert Model Inputs 
+    alpha, F_z, gamma, p_i = x
+    alpha = -np.radians(alpha)#NOTE this - sign is to convert the original 6.1 formula from adapted SAE to SAE coordinates
+    F_z = np.abs(F_z)
+    f_z0 = np.abs(f_z0)
+    gamma = np.radians(gamma)
+
+    C_t = np.abs(q_cz1) # 4.E41 NOTE abs might not be necessary 
+
+    B_t = (q_bz1 + q_bz2*5)
+    return 5
+
 
 
 def mf52_Fy(x, p, scaling_factors=None):
